2567cd1e477cf775258528a63e5159b6c6da6275
flyahn06 <flyahn06@gmail.com>
2023-10-13 12:49:23 +0900

    마이너 수정

--- server/dbhandler.py
+++ server/dbhandler.py
@@ -4,16 +4,41 @@ import pymongo
 config = configparser.ConfigParser()
 config.read("./conf.conf")
 
+
+class BaseDBError(Exception):
+    """Base class for ConfigParser exceptions."""
+
+    def __init__(self, msg=''):
+        self.message = msg
+        Exception.__init__(self, msg)
+
+    def __repr__(self):
+        return self.message
+
+    __str__ = __repr__
+
+
+class IDOverlapping(BaseDBError):
+    pass
+
+
+class IllegalStatus(BaseDBError):
+    pass
+
+
+class DocumentNotFound(BaseDBError):
+    pass
+
 class DBHandler:
     def __init__(self):
-        self.client = pymongo.MongoClient(f"mongodb://{config['DEFAULT']['ServerAddr']}:{config['DEFAULT']['ServerPort']}")
+        self.client = pymongo.MongoClient(f"mongodb://{config['DB']['ServerAddr']}:{config['DB']['ServerPort']}")
         self.db = self.client[config['DB']['ID']]
         self.collection = self.db[config['DB']['collection']]
 
     def insert_data(self, data):
         existing = self.find_data(data.get("id"))
         if existing:
-            return print("! | id 중복됨 (db반영X) \n")
+            raise IDOverlapping(f"{data}: id 중복됨")
 
         return self.collection.insert_one(data)
 
@@ -21,6 +46,9 @@ class DBHandler:
         return self.collection.find_one({"id": id})
 
     def update_status(self, id, new_status):
+        if not new_status in ["waiting", "done", "inprogress"]:
+            raise IllegalStatus(str(new_status))
+
         self.collection.update_one({"id": id}, {"$set": {"status": new_status}})
 
     def update_color(self, id, new_color):
